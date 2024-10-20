4467acfc3e98108a6826791e092a533f8ffe50d1
Oleg Frolyak <ofrolyak@gmail.com>
2024-10-20 12:36:00 +0300

    Some init staff

---
+++ src/main/java/com/tatko/api/TatkoSpringDemoBotBackEndApiApplication.java
@@ -0,0 +1,13 @@
+package com.tatko.TatkoSpringDemoBotBackEndApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatkoSpringDemoBotBackEndApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatkoSpringDemoBotBackEndApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tatko/api/TatkoSpringDemoBotBackEndApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatko.TatkoSpringDemoBotBackEndApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatkoSpringDemoBotBackEndApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
